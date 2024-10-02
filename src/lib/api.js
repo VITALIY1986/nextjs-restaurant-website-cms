7404a561a2f6a13bfc269283382a47ed4a61b0d0
VITALIY1986 <staslego1986@gmail.com>
2024-10-02 14:29:32 +0300

    asadsgdhshd

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,12 +1,1 @@
-import Cosmic from 'cosmicjs';
-
-const BUCKET_SLUG = process.env.COSMIC_BUCKET_SLUG
-const READ_KEY = process.env.COSMIC_READ_KEY
-
-const bucket = Cosmic().bucket({
-  slug: BUCKET_SLUG,
-  read_key: READ_KEY,
-});
-
-const is404 = (error) => /not found/i.test(error.message);
 
